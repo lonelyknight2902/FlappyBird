449d1168cd81658ac81bd46e694dd3d6e4574432
lonelyknight2902 <truongthinh2902@gmail.com>
2024-06-12 09:34:03 +0700

    refactor: Update player sprite rendering size

--- src/Player.ts
+++ src/Player.ts
@@ -6,13 +6,11 @@ class Player extends GameObject {
     private _sprite: HTMLImageElement
     private _spriteSource: string
     constructor() {
-        super(75, 50, BodyType.RIGID_BODY)
+        super(68, 48, BodyType.RIGID_BODY)
         console.log('Player created')
         this._sprite = document.createElement('img')
         this._spriteSource = 'assets/images/bluebird-upflap.png'
         this._sprite.src = this._spriteSource
-        this._sprite.width = 75
-        this._sprite.height = 50
     }
 
     set spriteSource(value: string) {
@@ -21,7 +19,7 @@ class Player extends GameObject {
 
     render(ctx: CanvasRenderingContext2D): void {
         const position = this.transform.getPosition()
-        ctx.drawImage(this._sprite, position.x, position.y, 75, 50)
+        ctx.drawImage(this._sprite, position.x, position.y, 68, 48)
     }
 
     flap(): void {
