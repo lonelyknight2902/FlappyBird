187c38b1e740172cf5e8e6eb24f4a30f50643dea
lonelyknight2902 <truongthinh2902@gmail.com>
2024-06-13 17:19:58 +0700

    refactor: Update Canvas class to handle background rendering

--- src/Canvas.ts
+++ src/Canvas.ts
@@ -1,15 +1,22 @@
+import { BACKGROUND_DAY } from "./constants"
+
 class Canvas {
     private _canvas: HTMLCanvasElement
+    private _background: HTMLImageElement
+    private _backgroundSource: string
     constructor(width: number, height: number) {
         this._canvas = document.createElement('canvas')
         this._canvas.width = width
         this._canvas.height = height
+        this._canvas.style.height = '100vh'
         const game = document.getElementById('game')
         if (game === null) {
             alert('Unable to find game element')
             return
         }
         game.appendChild(this.canvas)
+        this._background = document.createElement('img')
+        this._backgroundSource = BACKGROUND_DAY
     }
 
     get canvas(): HTMLCanvasElement {
@@ -20,15 +27,34 @@ class Canvas {
         this._canvas = value
     }
 
+    public getContext(): CanvasRenderingContext2D | null {
+        return this.canvas.getContext('2d')
+    }
+
     renderBackground() {
         const ctx = this.canvas.getContext('2d')
         if (ctx === null) {
             alert('Unable to get 2d context')
             return
         }
-        const image = new Image()
-        image.src = 'assets/images/background-day.png'
-        ctx.drawImage(image, 0, 0, this.canvas.width, this.canvas.height)
+        this._background.src = this._backgroundSource
+        ctx.drawImage(this._background, 0, 0, this.canvas.width, this.canvas.height)
+    }
+
+    public set backgroundSource(value: string) {
+        this._backgroundSource = value
+    }
+
+    public get backgroundSource(): string {
+        return this._backgroundSource
+    }
+
+    public clear() {
+        const ctx = this.canvas.getContext('2d')
+        if (ctx === null) {
+            return
+        }
+        ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
     }
 }
 
