ae716479cf25268a48e5a4dc1ada0c50fc817ad5
lonelyknight2902 <truongthinh2902@gmail.com>
2024-06-14 13:55:26 +0700

    refactor: Update ScoreManager to store and retrieve high score from local storage

--- src/ScoreManager.ts
+++ src/ScoreManager.ts
@@ -2,6 +2,15 @@ class ScoreManager {
     private _score = 0
     private _highScore = 0
 
+    constructor() {
+        const highScore = localStorage.getItem('highScore')
+        if (highScore !== null) {
+            this._highScore = parseInt(highScore)
+        } else {
+            localStorage.setItem('highScore', '0')
+        }
+    }
+
     get score() {
         return this._score
     }
@@ -13,6 +22,7 @@ class ScoreManager {
     public update() {
         if (this._score > this._highScore) {
             this._highScore = this._score
+            localStorage.setItem('highScore', this._highScore.toString())
         }
     }
 
