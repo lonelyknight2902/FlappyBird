e2b91fa6767b80216320842774cba83ab6275b97
lonelyknight2902 <truongthinh2902@gmail.com>
2024-06-14 13:54:20 +0700

    refactor: Update Canvas class to handle background rendering

--- src/Canvas.ts
+++ src/Canvas.ts
@@ -1,9 +1,13 @@
-import { BACKGROUND_DAY } from "./constants"
+import { BACKGROUND_DAY, BACKGROUND_NIGHT, DAY_NIGHT_CYCLE_TIME } from './constants'
+import UpdateInput from './types/update'
 
 class Canvas {
     private _canvas: HTMLCanvasElement
-    private _background: HTMLImageElement
-    private _backgroundSource: string
+    private _background: { [key: string]: HTMLImageElement }
+    private _currentBackground: string
+    private _start: number
+    private _currentAlpha: number
+    private _restart: boolean
     constructor(width: number, height: number) {
         this._canvas = document.createElement('canvas')
         this._canvas.width = width
@@ -15,8 +19,16 @@ class Canvas {
             return
         }
         game.appendChild(this.canvas)
-        this._background = document.createElement('img')
-        this._backgroundSource = BACKGROUND_DAY
+        const backgroundDay = document.createElement('img')
+        backgroundDay.src = BACKGROUND_DAY
+        const backgroundNight = document.createElement('img')
+        backgroundNight.src = BACKGROUND_NIGHT
+        this._background = {
+            day: backgroundDay,
+            night: backgroundNight,
+        }
+        this._currentBackground = 'day'
+        this._restart = true
     }
 
     get canvas(): HTMLCanvasElement {
@@ -31,22 +43,63 @@ class Canvas {
         return this.canvas.getContext('2d')
     }
 
+    public update(updateInput: UpdateInput): void {
+        let time: number
+        if (this._restart) {
+            this._start = Date.now()
+            time = 0
+            this._restart = false
+        } else {
+            time = (Date.now() - this._start) / DAY_NIGHT_CYCLE_TIME
+        }
+        if (time >= 1) {
+            this._start = Date.now()
+            this._currentBackground = this._currentBackground === 'day' ? 'night' : 'day'
+            time = 0
+        }
+        this._currentAlpha = (time * time) / (2.0 * (time * time - time) + 1.0)
+    }
+
+    public reset(): void {
+        this._start = Date.now()
+        this._currentBackground = 'day'
+        this._currentAlpha = 0
+        this._restart = true
+    }
+
     renderBackground() {
         const ctx = this.canvas.getContext('2d')
         if (ctx === null) {
             alert('Unable to get 2d context')
             return
         }
-        this._background.src = this._backgroundSource
-        ctx.drawImage(this._background, 0, 0, this.canvas.width, this.canvas.height)
+        ctx.save()
+        ctx.globalAlpha = this._currentAlpha
+        ctx.drawImage(
+            this._background[this._currentBackground === 'day' ? 'night' : 'day'],
+            0,
+            0,
+            this.canvas.width,
+            this.canvas.height
+        )
+        ctx.globalAlpha = 1 - this._currentAlpha
+        ctx.drawImage(
+            this._background[this._currentBackground],
+            0,
+            0,
+            this.canvas.width,
+            this.canvas.height
+        )
+
+        ctx.restore()
     }
 
-    public set backgroundSource(value: string) {
-        this._backgroundSource = value
+    public set currentBackground(key: string) {
+        this._currentBackground = key
     }
 
-    public get backgroundSource(): string {
-        return this._backgroundSource
+    public get currentBackground(): string {
+        return this._currentBackground
     }
 
     public clear() {
